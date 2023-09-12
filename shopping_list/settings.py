bd65e620fc7161714bcfa7941cf07c161614025d
arjunajaa <aarjunaja@gmail.com>
2023-09-12 19:36:41 +0700

    update settings

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^&e@t34@4$)y-oe8cr(f7sjhz7hoo*_9nc5^01dj&klf&mh^6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
